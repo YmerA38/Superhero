9b35e884b557d3c5081560dc5b7fd176432172f4
YmerA38 <stin166c@stud.kea.dk>
2022-09-12 15:40:00 +0200

    start

---
+++ src/main/java/Superhero.java
@@ -0,0 +1,12 @@
+public class Superhero {
+    private String name;
+    private String superheroName;
+    private int originYear;
+    private boolean isHuman;
+
+    public Superhero(String name,String superheroName,int originYear,boolean isHuman ){
+        this.name = name;
+        this.superheroName = superheroName;
+        this.originYear = originYear;
+    }
+}
