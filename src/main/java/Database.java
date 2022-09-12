4f44011e145212f4631789b064368de3fbd4bce9
YmerA38 <stin166c@stud.kea.dk>
2022-09-12 17:06:25 +0200

    der er nok noget der skal laves om

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,2 +1,26 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Database {
+
+    public void askForData(){
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("What is superheros suprhero name?");
+        String name = userInput.next();
+        System.out.println("What is superheros personal name?");
+        String superheroName = userInput.next();
+        System.out.println("What is superheros origin year ?");
+        int originYear = userInput.nextInt();
+        System.out.println("What is superheros a human  ?");
+        boolean isHuman = userInput.nextBoolean();
+
+
+    }
+
+
+
+
+
+
 }
--- src/main/java/Superhero.java
+++ src/main/java/Superhero.java
@@ -8,5 +8,23 @@ public class Superhero {
         this.name = name;
         this.superheroName = superheroName;
         this.originYear = originYear;
+        this.isHuman = isHuman;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getSuperheroName(){
+        return superheroName;
+    }
+    public int getOriginYear(){
+        return originYear;
+    }
+    public boolean getIsHuman(){
+        return isHuman;
+    }
+
+    public void setName(String Name){
+        this.name = name;
     }
 }
