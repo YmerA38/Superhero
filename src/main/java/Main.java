7694a0a09610ef3deb0c596b0d48d8623838202c
YmerA38 <stin166c@stud.kea.dk>
2022-09-13 09:59:29 +0200

    prøver med at opratte et superhero-array og lave en metode i Database til at gemme under index Virker tilsynel

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,17 +6,24 @@ public class Main {
 
         Database heroes = new Database();
         Scanner userInput = new Scanner(System.in);
-        System.out.println("What is superheros suprhero name?");
-        String superheroName = userInput.next();
-        System.out.println("What is superheros personal name?");
-        String name = userInput.next();
-        System.out.println("What is superheros origin year ?");
-        int originYear = userInput.nextInt();
-        System.out.println("What is superheros a human  ?");
-        boolean isHuman = userInput.nextBoolean();
+
+        int i =0;
+        do {
 
 
-        heroes.createSuperhero(superheroName,name,originYear,isHuman);
+            System.out.println("What is superheros suprhero name?");
+            String superheroName = userInput.next();
+            System.out.println("What is superheros personal name?");
+            String name = userInput.next();
+            System.out.println("What is superheros origin year ?");
+            int originYear = userInput.nextInt();
+            System.out.println("What is superheros a human  ?");
+            boolean isHuman = userInput.nextBoolean();
+            heroes.createSuperhero(superheroName, name, originYear, isHuman);
+            heroes.saveHeroToDatabase(i);
+            i++;
+
+        }while(i<100);
 
         System.out.println(heroes);
 
