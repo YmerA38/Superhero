3209c22acba12d09af6d4f4819d40dba66bba761
YmerA38 <stin166c@stud.kea.dk>
2022-09-15 10:32:14 +0200

    Har oprette en ny User interface klasse

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,51 +5,10 @@ public class Main {
 
     public static void main(String[] Args){
 
-        Database heroesDatabase = new Database();
-        Scanner userInput = new Scanner(System.in);
+        UserInterface uI = new UserInterface();
+        uI.startUserInterface();
 
-        heroesDatabase.createTestList(); //husk at slette, kun ti testbrug
-        int menu;
-        do {
-            System.out.println("Opret superhelt = 1, søg = 2, print = 3, Afslut = 0");
-            menu = userInput.nextInt();
-            if(menu==1) {
-                userInput.nextLine();//efter en ny nextInt bug
-                System.out.println("What is superheros suprhero name?");
-                String superheroName = userInput.nextLine();
-                System.out.println("What is superheros personal name?");
-                String name = userInput.nextLine();
-                System.out.println("What is heroes superpower?");
-                String superPower = userInput.next();
-                System.out.println("What is superheros origin year?");
-                int originYear = userInput.nextInt();
-                System.out.println("What is superheros a human?");
-                boolean isHuman = userInput.nextBoolean();
-                System.out.println("What is superheros strength (time a normal human)?");
-                double strength = userInput.nextDouble();
 
-                heroesDatabase.createSuperhero(superheroName,name,superPower,originYear,isHuman,strength);
-            }
-            if(menu == 2){
-                System.out.println("Indtast søgeord");
-                userInput.nextLine();//efter en ny nextInt bug
-                String search = userInput.nextLine();
-
-                System.out.println(heroesDatabase.searchSuperhero(search));
-            }
-            if(menu == 3){
-                int i = 0;
-                do{
-                    System.out.println("Superhelt: " + heroesDatabase.getSuperhero(i).getSuperheroName());
-                    System.out.println("Virkelige navn: " + heroesDatabase.getSuperhero(i).getName());
-                    System.out.println("Superkraft: "+ heroesDatabase.getSuperhero(i).getSuperPower());
-                    System.out.println("Oprindelsesår: " + heroesDatabase.getSuperhero(i).getOriginYear() + "\n"+
-                            "Styrketal: "+heroesDatabase.getSuperhero(i).getStrength()+"\n=====================");
-                    i++;
-                }while(i< heroesDatabase.getSuperheroList().size());
-            }
-
-        }while(menu != 0);
 
 
 
@@ -61,5 +20,7 @@ public class Main {
         // seach.equals(.....) er to strenge ens
         // .getNam().contais(search)
         // .getNam().toLowerCase.contais(search.toLowerCase)
+        //" ".equalsIgnorCase("")
+        // Integer.parseInt("")
     }
 }
---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class UserInterface {
+    Scanner userInput = new Scanner(System.in);
+    private Database heroesDatabase; // erklærer en instans
+
+    public UserInterface(){
+    }
+
+    public void startUserInterface(){
+        heroesDatabase = new Database(); // assigner instansen
+        heroesDatabase.createTestList(); //TODO husk at slette, kun til  testbrug
+        menu();
+    }
+
+    public void menu(){
+
+        int menu;
+        do {
+            menu = askUserInput();
+            if(menu==1) {
+                userDefinedSuperhero();
+            }
+            if(menu == 2){
+                searchDatabase();
+            }
+            if(menu == 3){
+                printAll();
+            }
+
+        }while(menu != 0);
+    }
+    private int askUserInput(){
+        System.out.println("Opret superhelt = 1, søg = 2, print = 3, Afslut = 0");
+        return userInput.nextInt();
+    }
+    private void userDefinedSuperhero(){
+        userInput.nextLine();//efter en ny nextInt bug
+        System.out.println("What is superheros suprhero name?");
+        String superheroName = userInput.nextLine();
+        System.out.println("What is superheros personal name?");
+        String name = userInput.nextLine();
+        System.out.println("What is heroes superpower?");
+        String superPower = userInput.next();
+        System.out.println("What is superheros origin year?");
+        int originYear = userInput.nextInt();
+        System.out.println("What is superheros a human?");
+        boolean isHuman = userInput.nextBoolean();
+        System.out.println("What is superheros strength (time a normal human)?");
+        double strength = userInput.nextDouble();
+        heroesDatabase.createSuperhero(superheroName,name,superPower,originYear,isHuman,strength);
+    }
+    private void searchDatabase(){
+        System.out.println("Indtast søgeord");
+        userInput.nextLine();//efter en ny nextInt bug
+        String search = userInput.nextLine();
+        System.out.println(heroesDatabase.searchSuperhero(search));
+    }
+    private void printAll(){
+        int i = 0;
+        do{
+            System.out.println("Superhelt: " + heroesDatabase.getSuperhero(i).getSuperheroName());
+            System.out.println("Virkelige navn: " + heroesDatabase.getSuperhero(i).getName());
+            System.out.println("Superkraft: "+ heroesDatabase.getSuperhero(i).getSuperPower());
+            System.out.println("Oprindelsesår: " + heroesDatabase.getSuperhero(i).getOriginYear() + "\n"+
+                    "Styrketal: "+heroesDatabase.getSuperhero(i).getStrength()+"\n=====================");
+            i++;
+        }while(i < heroesDatabase.getSuperheroList().size());
+    }
+
+
+
+}
